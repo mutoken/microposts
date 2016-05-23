42b9d5345013a0cb0c63351502e82691f63c6026
Kentaro Muto <kentaro.muto@gmail.com>
2016-05-23 17:25:41 +0000

    add user model

---
+++ db/migrate/20160523165218_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps null: false
+      
+      t.index :email, unique: true # この行を追加
+    end
+  end
+end
