afd82d7b9f9bb7bfb783533deb2cbe7dccbe4a01
Kentaro Muto <kentaro.muto@gmail.com>
2016-05-26 04:52:50 +0000

    added edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,20 +13,33 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #binding.pry
       flash[:success] = "Welcome to the sample app!"
       redirect_to @user
     else
       render 'new'
     end
-    #binding.pry
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    flash[:success] = "Updated!"
+  else
+    render edit
+  end
+  redirect_to @user
+  end
+
   
   private
   
   def user_params
     #binding.pry
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation, :location, :company, :profile)
   end
 
 
