ec37090a5aa45a51d7639d32952df3a0a1092992
Kentaro Muto <kentaro.muto@gmail.com>
2016-05-29 05:04:24 +0000

    merged user-profile

---
+++ db/migrate/20160529044902_create_microposts.rb
@@ -0,0 +1,11 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    create_table :microposts do |t|
+      t.references :user, index: true, foreign_key: true
+      t.text :content
+
+      t.timestamps null: false
+      t.index [:user_id, :created_at]
+    end
+  end
+end
